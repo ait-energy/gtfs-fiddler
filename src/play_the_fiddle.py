de3c454248805135c05d76ec2e7876b10897e05d
Markus Straub <markus.straub@ait.ac.at>
2024-01-18 14:08:28 +0100

    main script

--- src/play_the_fiddle.py
+++ src/play_the_fiddle.py
@@ -1,11 +1,12 @@
+"""
+Reduce GTFS feed to a single day and densify trips for that day
+"""
+
 import logging
 import sys
 from datetime import date
 from pathlib import Path
-
-import gtfs_kit as gk
-from gtfs_kit.miscellany import restrict_to_dates
-
+import argparse
 from gtfs_fiddler.fiddle import GtfsFiddler
 from gtfs_fiddler.gtfs_time import GtfsTime
 
@@ -15,19 +16,35 @@ logging.basicConfig(format=logFormat, datefmt="%Y-%m-%d %H:%M:%S", level=logging
 logger = logging.getLogger(__name__)
 
 
-def reduce_gtfs(in_file: Path, out_file: Path):
+def main(args):
+    print(args)
+    in_file = Path(args.in_gtfs)
+    out_file = Path(args.out_gtfs)
+    the_date = date.fromisoformat(args.date)
+
     logger.info(f"loading {in_file}")
-    feed = gk.read_feed(in_file, dist_units="m")
-    feed.describe()
-
-    fiddler = GtfsFiddler(in_file, "m")  # , date(2022, 6, 7))
+    logger.info(f"reducing it to {the_date}")
+    fiddler = GtfsFiddler(in_file, args.dist_unit, the_date)
+    logger.info(f"densify trips")
     fiddler.ensure_earliest_departure(GtfsTime("05:00"))
-
+    fiddler.ensure_latest_departure(GtfsTime("23:00"))
+    logger.info(f"writing result to {out_file}")
     fiddler.feed.write(out_file)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        "call with exactly two arguments: input and output gtfs.zip"
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("in_gtfs", type=str, help="path to input GTFS file")
+    parser.add_argument(
+        "date", type=str, help="single date (YYYY-MM-DD) the input GTFS is reduced to"
+    )
+    parser.add_argument("out_gtfs", type=str, help="path to output GTFS file")
+    parser.add_argument(
+        "--dist-unit",
+        type=str,
+        default="km",
+        help="distance unit (used in the input GTFS file)",
+    )
+    args = parser.parse_args()
 
-    reduce_gtfs(Path(sys.argv[1]), Path(sys.argv[2]))
+    main(args)
